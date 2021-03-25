d1d539ccc29fc8c5713182afeb1bd86e18f9186c
geogiadim <gia1914g@gmail.com>
2021-03-25 03:18:25 +0200

    create !addMatch, !printTable, !printMatchList commands

--- chessBot.py
+++ chessBot.py
@@ -1,7 +1,8 @@
+from tournament import Tournament
 from discord.ext.commands import Bot
 
 # write here your description and your command_prefix
-client = Bot(description="Chess Bot For SeraMG Invitational", command_prefix=">", pm_help=True)
+client = Bot(description="Chess Bot For SeraMG Invitational", command_prefix="!", pm_help=True)
 
 
 @client.event
@@ -10,9 +11,44 @@ async def on_ready():
 
 
 @client.command()
-async def hello(ctx):
-    await ctx.send("hi")
+async def addMatch(ctx):
+    await ctx.send("Write White's name")
+    white = await client.wait_for('message')
+    await ctx.send("Write Black's name")
+    black = await client.wait_for('message')
+    await ctx.send("Write Result")
+    result = await client.wait_for('message')
+    players = ['Kostino', 'Mesleh', 'Giannos', 'Hackerman', 'Kooustas', 'Billkapa', 'Aboosker']
+    tour = Tournament(players)
+    tour.addMatch(white.content, black.content, result.content)
+    tour.saveMatches()
+    pass
+
+"""
+@client.command()
+async def saveMatches(ctx):
+    players = ['Kostino', 'Mesleh', 'Giannos', 'Hackerman', 'Kooustas', 'Billkapa', 'Aboosker']
+    tour = Tournament(players)
+    tour.saveMatches()
+    pass
+"""
+
+
+
+@client.command()
+async def printTable(ctx):
+    players = ['Kostino', 'Mesleh', 'Giannos', 'Hackerman', 'Kooustas', 'Billkapa', 'Aboosker']
+    tour = Tournament(players)
+    await ctx.send(tour.prettyRanking())
+
+
+@client.command()
+async def printMatchList(ctx):
+    players = ['Kostino', 'Mesleh', 'Giannos', 'Hackerman', 'Kooustas', 'Billkapa', 'Aboosker']
+    tour = Tournament(players)
+    await ctx.send(tour.prettyMatchlist())
+
 
 # write here your discord bot token
-client.run('___YOUR TOKEN___')
+client.run('ODI0NDA4NTUyNzU5ODg1ODQ0.YFu8Sw.vck8G-LcNYXwZD5toic5GSXCw2o')
 
